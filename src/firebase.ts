5a1c12f1b5ca9db1fe3aa4f46ed614acf0c936a6
ahbsfy0905-ui <ahbsfy0905@gmail.com>
2026-03-25 20:31:06 +0900

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,16 +1,18 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+
 const firebaseConfig = {
   apiKey:            import.meta.env.VITE_FIREBASE_API_KEY,
   projectId:         import.meta.env.VITE_FIREBASE_PROJECT_ID,
   appId:             import.meta.env.VITE_FIREBASE_APP_ID,
   authDomain:        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseId:        import.meta.env.VITE_FIREBASE_DATABASE_ID,
   storageBucket:     import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
+
+// databaseId를 환경변수에서 직접 꺼내서 넘김
+export const db = getFirestore(app, import.meta.env.VITE_FIREBASE_DATABASE_ID);
